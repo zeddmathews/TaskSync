8e56180307e19196df5f7caad24c5643237db02f
Zain <zmathews@student.wethinkcode.co.za>
2025-05-19 13:40:32 +0200

    basic client interaction creating task

--- client/main.go
+++ client/main.go
@@ -1,9 +1,40 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"time"
+
+	taskpb "github.com/zeddmathews/tasksync/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 func main() {
+	conn, err := grpc.NewClient("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	// grpc.Dial("localhost:50051", grpc.WithInsecure()) **deprecated
+	if err != nil {
+		log.Fatalf("Failed to connect: %v", err)
+	}
+	defer conn.Close()
+
+	client := taskpb.NewTaskServiceClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	req := &taskpb.TaskRequest{
+		Title: "Test task",
+		Description: "Instance of task testing",
+	}
+
+	res, err := client.CreateTask(ctx, req)
+	if err != nil {
+		log.Fatalf("Error creating task: %v", err)
+	}
+
+	log.Printf("Task created. ID: %s, Status: %s\n", res.Id, res.Status)
+
 	fmt.Println("gRPC client running...")
 }
